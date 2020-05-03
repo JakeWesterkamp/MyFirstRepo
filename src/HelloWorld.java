ca2f64b9099f37a2b639ad6b7bbcc4ee109b67c5
Jake Westerkamp <westerjr@miamioh.edu>
2020-05-03 15:05:21 -0400

    My First commit in Git

---
+++ src/HelloWorld.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ * @author Jake Westerkamp
+ *
+ */
+public class HelloWorld {
+
+	/**
+	 * Main Method
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+	}
+
+}
